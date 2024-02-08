7b1981879d3a27dcda33dc05da0cc9d584158eb9
Eric Madureira <ericmadu416@gmail.com>
2024-02-08 16:37:28 -0300

    feat: fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World!'
+})
+
+app.listen({ port: 3616 }).then(() => {
+  console.log('HTTP Server running on http://localhost:3616')
+})
