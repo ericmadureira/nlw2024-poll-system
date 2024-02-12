6d64ad435c7ca6ccd38f98b196166d055d7a261c
Eric Madureira <ericmadu416@gmail.com>
2024-02-12 17:32:52 -0300

    feat: create poll with options

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -5,15 +5,29 @@ import { prisma } from '../../lib/prisma'
 
 export async function createPoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
+    // request body schema
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string())
     })
-    const { title } = createPollBody.parse(request.body)
+
+    const { title, options } = createPollBody.parse(request.body)
+
     const poll = await prisma.poll.create({
       data: {
-        title
+        title,
+        options: {
+          createMany: {
+            data: options.map(option => ({
+              title: option,
+              // Prisma will fill in the id automatically during creation, so we can ommit pollId here.
+              // pollId: poll.id
+            }))
+          }
+        }
       },
     })
+
     return reply.status(201).send({ pollId: poll.id })
   })
 }
