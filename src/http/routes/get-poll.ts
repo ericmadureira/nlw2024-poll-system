1af28cda7bd51f55b16082f21e694172235acba8
Eric Madureira <ericmadu416@gmail.com>
2024-02-13 18:00:11 -0300

    feat: get poll endpoint

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+import { prisma } from '../../lib/prisma'
+
+export async function getPoll(app: FastifyInstance) {
+  app.get('/polls/:pollId', async (request, reply) => {
+    // Schema for request query params
+    const createPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = createPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: { id: pollId },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true
+          }
+        }
+      }
+    })
+
+    return reply.status(200).send({ poll })
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3616 }).then(() => {
   console.log('HTTP Server running on http://localhost:3616')
