2f555694e3149176c37eedfcb4c2b558ca010e33
Eric Madureira <ericmadu416@gmail.com>
2024-02-13 18:27:53 -0300

    feat: setup @fastify/cookie

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,15 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'nlw24-secret',
+  hook: 'onRequest'
+})
 app.register(createPoll)
 app.register(getPoll)
 
