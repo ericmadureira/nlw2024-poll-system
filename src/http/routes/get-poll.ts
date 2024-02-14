70aecb3d877eacd1497c4648834047ebf33f9d18
Eric Madureira <ericmadu416@gmail.com>
2024-02-14 14:52:44 -0300

    feat: send current count in vote response

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -24,6 +25,30 @@ export async function getPoll(app: FastifyInstance) {
       }
     })
 
-    return reply.status(200).send({ poll })
+    if(!poll){
+      return reply.status(400).send({ message: 'Poll not found.' })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce((acc, line, index) => {
+      if(index % 2 === 0){
+        const score = Number(result[index+1])
+        Object.assign(acc, { [line]: score })
+      }
+      return acc
+    }, {} as Record<string, number>)
+
+    return reply.status(200).send({
+      poll: {
+        pollId: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => ({
+          optionId: option.id,
+          title: option.title,
+          votes: votes[option.id] || 0
+        })),
+      }
+    })
   })
 }
