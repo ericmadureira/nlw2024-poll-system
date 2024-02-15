704b1b94bdb7720ec69fa6bfb284ad0613e0010d
Eric Madureira <ericmadu416@gmail.com>
2024-02-15 10:44:25 -0300

    feat: register webscoket route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,21 +1,28 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import { fastifyWebsocket } from '@fastify/websocket'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { votePoll } from './routes/vote-poll'
+import { pollResults } from './websockets/poll-results'
 
 const app = fastify()
 
+// Handlers
 app.register(cookie, {
   secret: 'nlw24-secret',
   hook: 'onRequest'
 })
+app.register(fastifyWebsocket)
 
-// Routes
+// HTTP routes
 app.register(createPoll)
 app.register(getPoll)
 app.register(votePoll)
+
+// Websocket routes
+app.register(pollResults)
 
 app.listen({ port: 3616 }).then(() => {
   console.log('HTTP Server running on http://localhost:3616')
